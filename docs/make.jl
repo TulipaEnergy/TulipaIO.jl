7f1e4ccf43b6ed7b320554fe5b9ecc6dfb59029b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-02-12 17:38:27 +0100

    Update from copier template

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using TulipaIO
+using Documenter
+
+DocMeta.setdocmeta!(TulipaIO, :DocTestSetup, :(using TulipaIO); recursive = true)
+
+makedocs(;
+  modules = [TulipaIO],
+  doctest = true,
+  linkcheck = true,
+  authors = "Suvayu Ali <fatkasuvayu+linux@gmail.com> and contributors",
+  repo = "https://github.com/TulipaEnergy/TulipaIO.jl/blob/{commit}{path}#{line}",
+  sitename = "TulipaIO.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://TulipaEnergy.github.io/TulipaIO.jl",
+    assets = ["assets/style.css"],
+  ),
+  pages = [
+    "Home" => "index.md",
+    "Contributing" => "contributing.md",
+    "Dev setup" => "developer.md",
+    "Reference" => "reference.md",
+  ],
+)
+
+deploydocs(; repo = "github.com/TulipaEnergy/TulipaIO.jl", push_preview = true)
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+```@meta
+CurrentModule = TulipaIO
+```
+
+# TulipaIO
+
+Documentation for [TulipaIO](https://github.com/TulipaEnergy/TulipaIO.jl).
